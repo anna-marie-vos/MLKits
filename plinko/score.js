c52d6a31801db868548ed2cd11c3af25b06e3aa1
anna-marie-vos <anna@manu.net.nz>
2019-06-01 13:44:11 +1200

    plinko: first iteration of  k-nearest neighbors algorithm

--- plinko/score.js
+++ plinko/score.js
@@ -1,8 +1,32 @@
+const outputs = [];
+
+const point_x = 300;
+const k = 3;
+
+function similarity(point_y) {
+  const match = Math.abs(point_y - point_x);
+  return match;
+}
+
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
-  // Ran every time a balls drops into a bucket
+  outputs.push([dropPosition, bounciness, size, bucketLabel]);
 }
 
 function runAnalysis() {
-  // Write code here to analyze stuff
-}
+  const predictionResult = _.chain(outputs)
+    .map(row => {
+      const bucket = row[3];
+      return [similarity(row[0]), bucket];
+    })
+    .sortBy(row => row[0])
+    .slice(0, k)
+    .countBy(row => row[1])
+    .toPairs()
+    .sortBy(row => row[1])
+    .last()
+    .first()
+    .toString()
+    .value();
 
+  console.log(predictionResult);
+}
