2c5ab4d439e4b13fb44f7792070d0032375e2242
anna-marie-vos <anna@manu.net.nz>
2019-06-01 14:34:55 +1200

    plinko: splitting datasets to automatically test and compare data

--- plinko/score.js
+++ plinko/score.js
@@ -3,20 +3,25 @@ const outputs = [];
 const point_x = 300;
 const k = 3;
 
-function similarity(point_y) {
+function similarity(point_y, testPoint) {
+  const point_x = testPoint[0];
   const match = Math.abs(point_y - point_x);
   return match;
 }
 
-function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
-  outputs.push([dropPosition, bounciness, size, bucketLabel]);
+function splitDataset(data, testCount) {
+  const shuffledData = _.shuffle(data);
+
+  const testSet = _.slice(shuffledData, 0, testCount);
+  const trainingSet = _.slice(shuffledData, testCount);
+  return [testSet, trainingSet];
 }
 
-function runAnalysis() {
-  const predictionResult = _.chain(outputs)
+function knnAnalysis(data, testPoint) {
+  const predictedResult = _.chain(outputs)
     .map(row => {
       const bucket = row[3];
-      return [similarity(row[0]), bucket];
+      return [similarity(row[0], testPoint), bucket];
     })
     .sortBy(row => row[0])
     .slice(0, k)
@@ -25,8 +30,27 @@ function runAnalysis() {
     .sortBy(row => row[1])
     .last()
     .first()
-    .toString()
+    .toNumber()
     .value();
 
-  console.log(predictionResult);
+  return predictedResult;
+}
+
+function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
+  outputs.push([dropPosition, bounciness, size, bucketLabel]);
+}
+
+function runAnalysis() {
+  const testSetSize = 10;
+  const [testSet, trainingSet] = splitDataset(outputs, testSetSize);
+
+  let predictedCorrectly = 0;
+  testSet.map(testPoint => {
+    const bucket = knnAnalysis(trainingSet, testPoint);
+
+    if (bucket === testPoint[3]) {
+      predictedCorrectly++;
+    }
+  });
+  console.log({ accuracy: predictedCorrectly / testSetSize }, "%");
 }
